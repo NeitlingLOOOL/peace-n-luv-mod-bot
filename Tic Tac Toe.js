2f4b08469f41f79a8276d45b143752c1a58f3d50
Glitch (alpine-ionized-handstand) <none>
2022-02-07 13:44:33 +0000

    👀🎽 Checkpoint ./Tic Tac Toe.js:41973405/9254

--- Tic Tac Toe.js
+++ Tic Tac Toe.js
@@ -1,12 +1,34 @@
-const simplydjs = require('simply-djs')
+afk.js
 
-// message event
-if(command === 'tictactoe'){
-simplydjs.tictactoe(message, {
-    xEmoji: 'emoji id', //default: ❌
-    oEmoji: 'emoji id', //default: ⭕
-    idleEmoji: 'emoji id', //default: ➖
-    embedColor: 'hex code', //default: #075FFF
-    embedFoot: 'text for footer' //default: 'Make sure to win ;)' 
-})
+const db = require('quick.db')
+const { MessageEmbed } = require('discord.js')
+
+module.exports = {
+    name : 'afk',
+    run : async(client, message, args) => {
+        const reason = args.join(" ")
+        if(!reason) reason = no reason("he didn't tell me Lmao")
+        await db.set(`afk-${message.author.id}+${message.guild.id}`, reason)
+        const embed = new MessageEmbed()
+        .setDescription(`You have been set to afk\nReason : **${reason}**`)
+        .setColor("RANDOM")
+        .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic : true }))
+        message.channel.send(embed)                
+    }
 }
+
+index.js
+
+const  { MessageEmbed } = require('discord.js')
+client.on('message', async message => {
+if(db.has(`afk-${message.author.id}+${message.guild.id}`)) {
+        const afk = db.get(`afk-${message.author.id}+${message.guild.id}`)
+        await db.delete(`afk-${message.author.id}+${message.guild.id}`)
+        const embed = new MessageEmbed()
+        .setDescription(`Your afk status have been removed`)
+        .addField(`more info`, `${afk}`)
+        .setColor("RANDOM")
+        .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic : true }))
+        message.channel.send(embed)
+    }
+    })
