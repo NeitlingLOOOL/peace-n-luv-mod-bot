9aefe364201ac384735d0be2b0317b59fa66c3e1
NeitlingLOOOL <96809758+NeitlingLOOOL@users.noreply.github.com>
2024-12-13 12:46:31 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
-exports.Prefix = `v!`;//your prefix for bot
-exports.Token = `OTQwMjEzNTczNDI1MjAxMjIz.YgEIIA.Ts7UnV-58OSKR7aSAfvUpMusiGw`;//your token 
-exports.Color = `BLUE`;//color   of embed
+exports.Prefix = `,`;//your prefix for bot
+exports.Token = `MTMxNzE3ODYzNjY1MDAyNTA0MA.GenRRn.CwFd6tWlvyfai2Gd2PGlQONq9GC3BzO-yJo1go
+`;//your token 
+exports.Color = `RED`;//color   of embed
